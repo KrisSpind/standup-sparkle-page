849b6eab09b5175824786cfc0a44cec2b6a81a38
Kristina Spindler <160973608+KrisSpind@users.noreply.github.com>
2026-03-19 22:30:16 +0100

    Set basename for BrowserRouter in App component

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/standup-sparkle-page/">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
